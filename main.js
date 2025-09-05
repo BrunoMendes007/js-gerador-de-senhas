c18b28aa81cde81608c80340a44b92c34e26dda2
BrunoMendes007 <bruno.neduziak.mendes@escola.pr.gov.br>
2025-09-05 12:09:41 +0000

    onclick

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 const numeroSenha = document.querySeletor('parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySeletorAll('parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminiuTamanho(){
+    if (tamanhoSenha >1){
+        //tamanhoSenha = tamanhoSenha - 1
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+        //tamanhoSenha = tamanhoSenha - 1
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+

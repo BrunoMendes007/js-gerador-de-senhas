cd63bcc1b1c430baba6f07caadb157d59f0ea547
BrunoMendes007 <bruno.neduziak.mendes@escola.pr.gov.br>
2025-09-12 10:45:28 +0000

    random

--- main.js
+++ main.js
@@ -22,3 +22,73 @@ function aumentaTamanho(){
     numeroSenha.textContent = tamanhoSenha;
 }
 
+const numeroSenha = document.querySelector('parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if (tamanhoSeha > 1){
+        //tamanhoSenha = tamanhoSenha - 1
+        tamanhoSenha --;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+        //tamanhoSenha = tamanhoSenha + 1
+        tamanhoSenha ++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function geraSenha(){
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha){
+
+    }
+}
+const numeroSenha = document.querySelector('parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if (tamanhoSeha > 1){
+        //tamanhoSenha = tamanhoSenha - 1
+        tamanhoSenha --;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+        //tamanhoSenha = tamanhoSenha + 1
+        tamanhoSenha ++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function geraSenha(){
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha){
+
+    }
+}
